ba5f9c3a41c7f2cb935d92f31b4bbad91cee935f
Brian Burttram <brian.burttram@pinnacol.com>
2018-09-18 10:03:13 -0600

    Plumbing for click handling

--- src/Quiz.js
+++ src/Quiz.js
@@ -22,10 +22,15 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
     );
+  }
+
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
   }
 }
 export default Quiz;
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,9 +4,18 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>
+          {this.props.button_text}
+        </button>
       </li>
     );
+  }
+
+  handleClick(buttonText) {
+    this.props.clickHandler();
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
   }
 }
 export default QuizQuestionButton;
